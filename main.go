2c0e8079a1b3f21d5fe3750a44285f1603a633b8
Rahul-D78 <rahul.kumar@aveshasystems.com>
2024-01-04 11:56:09 +0530

    fix(): handle webhook creation through env var
    Signed-off-by: Rahul-D78 <rahul.kumar@aveshasystems.com>

--- main.go
+++ main.go
@@ -371,18 +371,21 @@ func main() {
 		}()
 	}
 
-	var registerClient = k8s.AdmissionWebhookRegisterClient{
-		Logger: logger.Named("admissionWebhookRegisterClient"),
-	}
+	// Deploy mutating webhook manifest
+	if conf.RegisterWebhookClient {
+		var registerClient = k8s.AdmissionWebhookRegisterClient{
+			Logger: logger.Named("admissionWebhookRegisterClient"),
+		}
 
-	err = registerClient.Register(ctx, conf)
-	if err != nil {
-		prod.Fatal(err.Error())
-	}
+		err = registerClient.Register(ctx, conf)
+		if err != nil {
+			prod.Fatal(err.Error())
+		}
 
-	defer func() {
-		_ = registerClient.Unregister(context.Background(), conf)
-	}()
+		defer func() {
+			_ = registerClient.Unregister(context.Background(), conf)
+		}()
+	}
 
 	s := echo.New()
 	s.Use(middleware.Logger())
